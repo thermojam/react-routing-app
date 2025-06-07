2f873ede835d5e973777e8e19a96b36b1b358169
Nikita Mensky <nmensky@gmail.com>
2025-06-07 23:08:33 +0300

    project initialization and first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+export const App = () => {
+
+
+  return (
+      <>
+          <h1 className="text-8xl font-bold mb-6 text-center text-cyan-400">Hello Todos</h1>
+      </>
+  )
+}
+
+
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+    server: {
+      open: true,
+        port: 3000
+    }
+})
